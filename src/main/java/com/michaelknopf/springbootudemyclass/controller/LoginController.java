1bb12a0b4d5f27220137471cb8892e4f55a8f79f
Michael_Knopf <mikeknopf2@gmail.com>
2021-10-15 00:29:50 -0400

    add userID param and pass to jsp

--- src/main/java/com/michaelknopf/springbootudemyclass/controller/LoginController.java
+++ src/main/java/com/michaelknopf/springbootudemyclass/controller/LoginController.java
@@ -1,14 +1,20 @@
 package com.michaelknopf.springbootudemyclass.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 	
+	//Model
+	
+	
 	@RequestMapping("/login")
-	public String loginMessage() {
+	public String loginMessage(@RequestParam String name, ModelMap model) {
+		model.put("name", name);
 		return "login";
 	}
 }
--- src/main/webapp/WEB-INF/jsp/login.jsp
+++ src/main/webapp/WEB-INF/jsp/login.jsp
@@ -4,11 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./login.css">
     <title>First Web Application</title>
 </head>
 <body>
     
     <h1>My First JSP</h1>
+    <h3>Welcome ${name}!</h3>
 
 </body>
 </html>
